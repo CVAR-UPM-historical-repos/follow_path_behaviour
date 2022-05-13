47ca9d0250f79c612eb70a56645a1552d1692691
perezsaura-david <perezsaura.david@gmail.com>
2022-05-13 10:57:15 +0200

    v0.1.0

--- follow_path_plugin_base/include/follow_path_plugin_base/follow_path_base.hpp
+++ follow_path_plugin_base/include/follow_path_plugin_base/follow_path_base.hpp
@@ -1,3 +1,39 @@
+/*!*******************************************************************************************
+ *  \file       follow_path_base.hpp
+ *  \brief      follow_path_base header file
+ *  \authors    Miguel Fernández Cortizas
+ *              Pedro Arias Pérez
+ *              David Pérez Saura
+ *              Rafael Pérez Seguí
+ *
+ *  \copyright  Copyright (c) 2022 Universidad Politécnica de Madrid
+ *              All Rights Reserved
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ********************************************************************************/
+
 #ifndef FOLLOW_PATH_BASE_HPP
 #define FOLLOW_PATH_BASE_HPP
 
@@ -53,7 +89,8 @@ namespace follow_path_base
 
             this->actual_speed_ = Eigen::Vector3d(msg->twist.twist.linear.x,
                                                   msg->twist.twist.linear.y,
-                                                  msg->twist.twist.linear.z).norm();
+                                                  msg->twist.twist.linear.z)
+                                      .norm();
         };
 
     protected:
@@ -72,6 +109,6 @@ namespace follow_path_base
         rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
     }; // FollowPathBase class
 
-}  // follow_path_base namespace
+} // follow_path_base namespace
 
 #endif // FOLLOW_PATH_BASE_HPP
